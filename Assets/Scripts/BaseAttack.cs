7b688727a4d03aa0968151a196031ece01cfacce
傑德 <as1942565@gmail.com>
2026-01-31 11:22:17 +0800

    玩家移動與攻擊雛形

---
+++ Assets/Scripts/BaseAttack.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+
+public class BaseAttack : MonoBehaviour
+{
+    private SpriteRenderer sprite;
+
+    private void Awake()
+    {
+        sprite = GetComponent<SpriteRenderer>();
+
+        Destroy(gameObject , 0.5f);
+    }
+    private void FixedUpdate()
+    {
+        var tempColor = sprite.color;
+        tempColor.a -= 2f * Time.fixedDeltaTime;
+        if (tempColor.a < 0)
+            tempColor.a = 0;
+        sprite.color = tempColor;
+    }
+}
